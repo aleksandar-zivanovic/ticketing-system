4dc5d3fa932387ab1952269b94e77493e444bf4c
Aleksandar Zivanovic <acas_mail@yahoo.com>
2024-10-23 15:18:13 +0200

    Created connection with database

---
+++ classes/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database
+{
+    private $username = "root";
+    private $password = "";
+    private $dbName = "ticketing-system";
+    private $host = "localhost";
+    private $conn = null;
+    
+    public function connect()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbName", $this->username, $this->password);
+            // set the PDO error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+          }
+
+        return $this->conn;
+    }
+
+}
