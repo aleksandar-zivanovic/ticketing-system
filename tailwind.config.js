b46d6a29b65570609aca505ee5e87eeef9caf30b
Aleksandar Zivanovic <acas_mail@yahoo.com>
2024-11-08 16:55:58 +0100

    chore: update npm scripts and Tailwind config for development and production setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js,php}'],
+  content: ['./public/**/*.{html,js,php}', './partials/**/*.{html,js,php}', './forms/**/*.{html,js,php}', ],
   theme: {
     extend: {},
   },
