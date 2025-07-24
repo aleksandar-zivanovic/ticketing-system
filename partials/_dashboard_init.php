893db69936febae0af9602fc8df3524a40539efc
Aleksandar Zivanovic <acas_mail@yahoo.com>
2025-07-24 10:57:26 +0200

    refactor(dashboard): move page rendering to partial - render dashboards via dashboard_init.php

---
+++ partials/_dashboard_init.php
@@ -0,0 +1,81 @@
+<?php
+require_once '../../helpers/admin/dashboard_controller.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" class="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $page ?></title>
+
+    <link rel="stylesheet" href="../css/admin-one-main.css">
+    <!-- Tailwind is included -->
+    <link rel="stylesheet" href="../css/tailwind-output.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <!-- Chart.js-->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+</head>
+
+<body>
+    <div id="app">
+        <?php
+
+        // import header navigation bar
+        include_once '../../partials/_navigation-bar.php';
+
+        // import side menu
+        include_once '../../partials/_side-menu.php';
+
+        // import breadcrumbs
+        include_once '../../partials/_navigation-breadcrumbs.php';
+
+        include_once '../../partials/_dashboard_hero.php';
+        ?>
+
+        <section class="section main-section">
+            <?php
+            // Statistic for all tickets
+            $cardGroup = "all";
+            require '../../helpers/admin/dashboard_cards.php';
+
+            if ($panel === "admin") {
+                // Statistic for tickets handled by the administrator
+                $cardGroup = "handling";
+                require '../../helpers/admin/dashboard_cards.php';
+            }
+            ?>
+
+            <!-- Charts -->
+            <?php
+            $commonChartLabel = $panel === "admin" ? "All tickets chart" : "Your tickets chart";
+            renderChart($commonChartLabel, "line", $chartAllData);
+            if ($panel === "admin") {
+                renderChart("Tickets you are handling", "line", $chartHandledData);
+            }
+            ?>
+
+            <!-- Tables -->
+            <div class="card has-table grid grid-cols-1 gap-6 lg:grid-cols-2 mb-6">
+                <?php
+                foreach ($arrayTables as $category  => $items) {
+                    $pieces = explode(' ', $category);
+                    $columnName = array_pop($pieces);
+                    include '../../partials/_admin_dashboard_table.php';
+                }
+                ?>
+        </section>
+
+        <?php
+
+        // import footer
+        include_once '../../partials/_footer.php';
+        ?>
+    </div>
+
+</body>
+
+</html>
